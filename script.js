475bec7dfd3e3389611aac34973ef6f9e24fee38
Jackson Porciúncula <jbrunops@outlook.com>
2025-01-08 12:36:34 -0300

    Selecao das celulas do tabuleiro adicionadas

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ for (let i = 0; i < 9; i++) {
   celula.classList.add("celula");
   tabuleiro.appendChild(celula);
 }
+
+let jogadorAtual = "x";
+const celulas = document.querySelectorAll("#tabuleiro div");
+
+celulas.forEach((celula) => {
+  celula.addEventListener("click", () => {
+    if (celula.textContent === "") {
+      celula.textContent = jogadorAtual;
+      jogadorAtual = jogadorAtual === "x" ? "o" : "x";
+    }
+  });
+});
