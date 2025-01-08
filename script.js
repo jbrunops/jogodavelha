8bb9d18b1b1ec5e1e60ed23efe92cd8db4341d6c
Jackson Porciúncula <jbrunops@outlook.com>
2025-01-08 12:44:49 -0300

    Funcção de verificação de vitória adicionada

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ celulas.forEach((celula) => {
     }
   });
 });
+
+function verificarVitoria() {
+  const combinacoesVencedoras = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let combinacao of combinacoesVencedoras) {
+    const [a, b, c] = combinacao;
+
+    if (
+      celulas[a].textContent !== "" &&
+      celulas[a].textContent === celulas[b].textContent &&
+      celulas[a].textContent === celulas[c].textContent
+    ) {
+      return celulas[a].textContent;
+    }
+  }
+}
+
